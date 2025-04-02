5b1a840b2387856fa9cdcfba5b8234d391def48a
seu nome <teste@exemplo.com>
2025-04-02 15:12:47 -0300

    adiciona post e get da api

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,19 +1,35 @@
 import livroProtagonista from '../../assets/livroProtagonista.png'
 import s from './livrosDoados.module.scss'
+import axios from 'axios'
+import {useState,useEffect} from 'react'
 
 export default function LivrosDoados(){
+
+    const [livros,setLivros] = useState([])
+
+    const getLivros = async() => {
+       const resposta = await axios.get('https://api-livros-doados.onrender.com/livros') 
+       setLivros(resposta.data)
+    }
+
+    useEffect(()=>{
+        getLivros()
+    },[])
+
     return(
         <section className={s.livrosDoadosSection}>
             <h2>Livros Doados</h2>
             <section className={s.containerCards}>
-                <section>
-                    <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
-                    <div>
-                        <h3>O protagonista</h3>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
+                {
+                   livros.map((item)=>(
+                    <section>
+                        <img src={item.imagem_url} alt={`Titulo do livro ${item.titulo}`} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </section>
+                   )) 
+                }
             </section>
         </section>
     )
